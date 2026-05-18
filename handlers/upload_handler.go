5e0dea52c61615de7eedf4e67e1ca1c63d3b7b2b
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-05-18 18:36:28 +0530

    updated pdf viewer logics

--- handlers/upload_handler.go
+++ handlers/upload_handler.go
@@ -55,6 +55,40 @@ func (h *UploadHandler) ProxyPDF(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "id is required"})
 		return
 	}
+	// If client sent a Google OAuth access token, use it to fetch the file on behalf of the user
+	authHeader := c.GetHeader("Authorization")
+	if authHeader != "" {
+		// expect format: "Bearer <token>"
+		req, err := http.NewRequest("GET", fmt.Sprintf("https://www.googleapis.com/drive/v3/files/%s?alt=media", id), nil)
+		if err == nil {
+			req.Header.Set("Authorization", authHeader)
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err == nil {
+				defer resp.Body.Close()
+				if resp.StatusCode == http.StatusOK {
+					if c.Query("download") != "" {
+						c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", id))
+					} else {
+						c.Header("Content-Disposition", "inline")
+					}
+					if ct := resp.Header.Get("Content-Type"); ct != "" {
+						c.Header("Content-Type", ct)
+					} else {
+						c.Header("Content-Type", "application/octet-stream")
+					}
+					c.Status(http.StatusOK)
+					io.Copy(c.Writer, resp.Body)
+					return
+				}
+				// pass through status/body if not OK
+				c.Status(resp.StatusCode)
+				io.Copy(c.Writer, resp.Body)
+				return
+			}
+		}
+		// if any error using auth header, fall through to other methods
+	}
 	// If a Google service account key path is provided via env, use it to fetch private Drive files
 	keyPath := os.Getenv("GOOGLE_SERVICE_ACCOUNT_KEY")
 	if keyPath != "" {

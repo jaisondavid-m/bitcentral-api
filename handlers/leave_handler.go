a4a08320c009c80417c08397f30642bda7b998d7
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-07-19 14:59:42 +0530

    updated leave details

--- handlers/leave_handler.go
+++ handlers/leave_handler.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"server/data"
+	"server/models"
 )
 
 type LeaveHandler struct{}
@@ -14,9 +16,21 @@ func NewLeaveHandler() *LeaveHandler {
 }
 
 func (h *LeaveHandler) GetAllLeaves(c *gin.Context) {
+	processedHolidays := make([]models.Holiday, len(data.Holidays))
+	for i, hol := range data.Holidays {
+		processedHolidays[i] = hol
+		if strings.HasSuffix(hol.FromDate, "(AN)") {
+			processedHolidays[i].FromDate = strings.TrimSuffix(hol.FromDate, "(AN)")
+			processedHolidays[i].FromHalfDay = "AN"
+		} else if strings.HasSuffix(hol.FromDate, "(FN)") {
+			processedHolidays[i].FromDate = strings.TrimSuffix(hol.FromDate, "(FN)")
+			processedHolidays[i].FromHalfDay = "FN"
+		}
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"count":   len(data.Holidays),
-		"data":    data.Holidays,
+		"count":   len(processedHolidays),
+		"data":    processedHolidays,
 	})
 }
